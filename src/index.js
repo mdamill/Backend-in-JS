c71668dccf9364d95f8b075209f3c92f11b69a7c
mdamill <losthts8@gmail.com>
2025-08-29 19:44:50 +0530

    initial controller and routes tested using postman

--- src/index.js
+++ src/index.js
@@ -36,13 +36,10 @@ import {DB_NAME} from './constants.js'
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-// import {app} from './app.js'
-import express from 'express'
+import {app} from './app.js'
 dotenv.config({
     path: './.env'
 })
-
-const app = express()
 
 connectDB()
 .then(() => {
